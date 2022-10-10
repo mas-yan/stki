76f4070af3f5ebf52ee9cd9c8152f68a74dd1e0b
mas-yan <mrey5597@gmail.com>
2022-10-10 10:28:16 +0700

    initial commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'sastrawi/sastrawi' => array(
+            'pretty_version' => 'v1.0.0',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../sastrawi/sastrawi',
+            'aliases' => array(),
+            'reference' => 'fd3cd29e46954b2c8f24dc95f029ce233871d2ef',
+            'dev_requirement' => false,
+        ),
+    ),
+);
